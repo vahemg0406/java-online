1ee93c3c8e4fc197f1f6ce32313c95201855c7b3
vahemg0406 <vahemg4466@gmail.com>
2022-05-09 22:56:16 +0400

    First project

---
+++ Lesson1/src/Lesson1.java
@@ -0,0 +1,21 @@
+public class Lesson1 {
+    public static void main(String[] args) {
+        System.out.print("babam\n");
+
+        long x;
+        x=5441146846849848l;
+        System.out.println(x);
+
+        float a=2.5f;
+        double s=456.22;
+        boolean f= true;
+        f=false;
+        System.out.println(f);
+
+        char h ='%';
+        h = '\u006f';
+        System.out.println(h);
+        String tx = "hellooo";
+        System.out.println(tx);
+    }
+}
